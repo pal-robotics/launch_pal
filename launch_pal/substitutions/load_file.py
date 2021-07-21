ba5f95040f948247ee0e924a4f8f0808f544f38c
Victor Lopez <victor.lopez@pal-robotics.com>
2021-07-21 10:47:36 +0200

    Linter fixes

--- launch_pal/substitutions/load_file.py
+++ launch_pal/substitutions/load_file.py
@@ -12,26 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections.abc
+from collections.abc import Iterable
 from launch.launch_context import LaunchContext
 from launch.substitution import Substitution
 from launch.some_substitutions_type import SomeSubstitutionsType
-from launch.utilities import normalize_to_list_of_substitutions, ensure_argument_type, perform_substitutions
+from launch.utilities import (normalize_to_list_of_substitutions, ensure_argument_type,
+                              perform_substitutions)
 
 from typing import Text
 
 
 class LoadFile(Substitution):
-    """
-    The file_path may contain Substitutions, they are resolved and concatenated.
-    """
+    """The file_path may contain Substitutions, they are resolved and concatenated."""
+
     def __init__(self, file_path: SomeSubstitutionsType) -> None:
         """Create a LoadFile substitution."""
         super().__init__()
 
         ensure_argument_type(
             file_path,
-            (str, Substitution, collections.abc.Iterable),
+            (str, Substitution, Iterable),
             'file_path',
             'LoadFile')
 
