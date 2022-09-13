3bc69416bc0b18ce1f826f4802c857b8c3a68433
Noel Jimenez <noel.jimenez@pal-robotics.com>
2022-09-13 18:19:43 +0200

    rm unused import

--- launch_pal/robot_utils.py
+++ launch_pal/robot_utils.py
@@ -12,9 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import PythonExpression
 
+from launch.actions import DeclareLaunchArgument
 
 def get_robot_name():
     declare_robot_name = DeclareLaunchArgument(
