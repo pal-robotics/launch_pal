b6557e38498c4936192e12a79592c50f8acfdd2d
David ter Kuile <david.terkuile@pal-robotics.com>
2023-11-15 17:12:24 +0100

    Add base dataclass with for launch args

--- launch_pal/arg_utils.py
+++ launch_pal/arg_utils.py
@@ -12,10 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.utilities import perform_substitutions
 from launch.actions import DeclareLaunchArgument
 from launch_pal.robot_config import RobotSetting
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True, kw_only=True)
+class LaunchArgumentsBase:
+    def __init_subclass__(cls, **kwargs):
+        annotations = getattr(cls, '__annotations__', {})
+        for attr, type_ in annotations.items():
+            if not issubclass(type_, DeclareLaunchArgument):
+                raise TypeError(
+                    f"All attributes in {cls.__name__} must have type DeclareLaunchArgument")
+
+    def add_to_launch_description(self, launch_description: LaunchDescription):
+        annotations = getattr(self, '__annotations__', {})
+        for attr, type_ in annotations.items():
+            launch_description.add_action(getattr(self, attr))
+        return
 
 
 def read_launch_argument(arg_name, context):
