bdea898f3b07d713f4eeb6d832e09285429ab959
David ter Kuile <david.terkuile@pal-robotics.com>
2024-06-20 09:33:14 +0200

    ArgFactory class to create launch args from yaml

--- launch_pal/arg_utils.py
+++ launch_pal/arg_utils.py
@@ -17,7 +17,62 @@ from launch.substitutions import LaunchConfiguration
 from launch.utilities import perform_substitutions
 from launch.actions import DeclareLaunchArgument
 from dataclasses import dataclass
+import yaml
 
+
+class LaunchArgCreator:
+
+    def __init__(self) -> None:
+        self.launch_arguments = {}
+
+    def from_yaml(self, yaml_file: str) -> dict:
+        with open(yaml_file, 'r') as file:
+            arg_configurations = yaml.load(file, Loader=yaml.FullLoader)
+            try:
+                self.launch_arguments = {key: self.dict_to_launch_arg(
+                    item, key) for key, item in arg_configurations.items()}
+            except Exception as e:
+                print(f"Could not parse launch argument from file {yaml_file}")
+                print(f"Error: {e}")
+
+    def is_valid_dict(self, launch_dict: dict) -> bool:
+        valid_keys = ['description', 'default_value', 'choices']
+        key_set = set(launch_dict.keys())
+
+        # Ensure description is in the dictionary and no invalid keys are present
+        return ('description' in key_set) and key_set.issubset(valid_keys)
+
+    def dict_to_launch_arg(self, launch_dict: dict, arg_name: str) -> str:
+
+        if not self.is_valid_dict(launch_dict):
+            raise ValueError(
+                f"Launch argument configuration is not valid for {arg_name}")
+
+        description = launch_dict['description']
+        default_value = None
+        choices = None
+        if 'default_value' in launch_dict.keys():
+            default_value = launch_dict['default_value']
+        if 'choices' in launch_dict.keys():
+            choices = launch_dict['choices']
+
+        return DeclareLaunchArgument(name=arg_name,
+                                     description=description,
+                                     default_value=default_value,
+                                     choices=choices)
+
+    def get_argument(self, arg_name: str) -> DeclareLaunchArgument:
+        if arg_name not in self.launch_arguments.keys():
+            raise KeyError(
+                f"Launch argument {arg_name} does not exist")
+
+        return self.launch_arguments[arg_name]
+
+
+def parse_launch_args_from_yaml(yaml_file: str) -> LaunchArgCreator:
+    launch_args = LaunchArgCreator()
+    launch_args.from_yaml(yaml_file)
+    return launch_args
 
 
 @dataclass(frozen=True, kw_only=True)
